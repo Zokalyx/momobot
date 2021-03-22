b825dee7555c13f7707d67a25a4b6bdc42b4b2f5
Zokalyx <zokalyx@gmail.com>
2021-03-22 02:04:49 -0300

    asd

--- users.js
+++ users.js
@@ -68,8 +68,8 @@ packInfoFromUser(id = undefined, users, cards, config, pack, cache = undefined, 
 
             return (
 
-                actualUserCollection.map( v => `**#${v+1} ${ ca[v][5]?`"${ca[v][5]}"`:"" }** - Valor: $${ca[v][3]} - Multiplicador: x${ca[v][4]}`).join("\n")
-                + `\n\n**Valor total:** $${total} - **Valor promedio:** $${avg} - ${ca[v][1]}`
+                actualUserCollection.map( v => `**#${v+1} ${ ca[v][5]?`"${ca[v][5]}"`:"" }** - Valor: $${ca[v][3]} - Multiplicador: x${ca[v][4]} - ${ca[v][1]}`).join("\n")
+                + `\n\n**Valor total:** $${total} - **Valor promedio:** $${avg}`
 
             );
 
